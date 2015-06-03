bbaa52d16218eec94ad0253a0762f6b8e4335722
wiggins <wiggins2000@gmail.com>
2015-06-03 13:12:15 +0200

    Added click listener and toast response on button clicks to the MainActivity.java

--- app/src/main/java/com/udacity/wiggins/myappportfolio/MainActivity.java
+++ app/src/main/java/com/udacity/wiggins/myappportfolio/MainActivity.java
@@ -1,9 +1,13 @@
 package com.udacity.wiggins.myappportfolio;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +16,79 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.btnSpotify);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "This button will launch my Spotify App!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+
+        Button ScoresButton = (Button) findViewById(R.id.btnScores);
+        ScoresButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "This button will launch my Score App!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+
+        Button LibraryButton = (Button) findViewById(R.id.btnLibrary);
+        LibraryButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "This button will launch my Library App!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+
+        Button BiggerButton = (Button) findViewById(R.id.btnBigger);
+        BiggerButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "This button will launch my Build It Bigger App!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+
+        Button ReaderButton = (Button) findViewById(R.id.btnReader);
+        ReaderButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "This button will launch my Reader App!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+
+        Button CapstoneButton = (Button) findViewById(R.id.btnCapstone);
+        CapstoneButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "This button will launch my Capstone App!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+
     }
 
     @Override
